c44ec5e297e4880de2917783a7ce0283d8a5b067
jeandejesus <jean_freire2@hotmail.com>
2022-03-13 00:59:40 -0300

    estrutura pagina artista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ArtistaComponent
   ],
   imports: [
     BrowserModule,
