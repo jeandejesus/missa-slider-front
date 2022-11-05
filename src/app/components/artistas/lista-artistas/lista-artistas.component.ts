854ec3dab7de79f201fb04ea1f53a1ce3a42c1b7
C1322975 Jean de Jesus Freire <C1232975@interno.bb.com.br>
2022-11-05 16:08:48 -0300

    ajustes layout

--- src/app/components/artistas/lista-artistas/lista-artistas.component.ts
+++ src/app/components/artistas/lista-artistas/lista-artistas.component.ts
@@ -1,21 +1,27 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+import {
+  faEllipsisVertical,
+  faTrash,
+  faPencil,
+} from '@fortawesome/free-solid-svg-icons';
 import { Artista } from '../../../shared/models/artista';
 
 @Component({
   selector: 'app-lista-artistas',
   templateUrl: './lista-artistas.component.html',
-  styleUrls: ['./lista-artistas.component.scss']
+  styleUrls: ['./lista-artistas.component.scss'],
 })
 export class ListaArtistasComponent implements OnInit {
-
   faCofaEllipsisVertical = faEllipsisVertical;
+  faTrash = faTrash;
+  faPencil = faPencil;
+
   @Input()
-  artista : Artista = {}
+  artista: Artista = {};
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  deletar(): void {}
 }
